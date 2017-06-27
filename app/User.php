917dbf08ca71298a4c6582a437d58b8967bee5bd
Sam Greenwood <sam@samgreenwood.me>
2017-06-28 00:21:36 +0930

    Models and Migrations
    Create migrations and models for
    
    User - User of the system
    Repository - Git repository to use to create a post
    Post - A reference to a repository and a commit hash to diff against
    master to generate the post

--- app/User.php
+++ app/User.php
@@ -9,21 +9,32 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function repositories()
+    {
+        return $this->hasMany(Repository::class);
+    }
+
+    public function addRepository(Repository $repository)
+    {
+        return $this->repositories()->save($repository);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function addPost(Post $post)
+    {
+        return $this->posts()->save($post);
+    }
+
 }
---
+++ database/migrations/2017_06_27_143051_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedInteger('repository_id');
+            $table->longText('generated_markdown');
+            $table->longText('edited_markdown');
+            $table->string('hash');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
