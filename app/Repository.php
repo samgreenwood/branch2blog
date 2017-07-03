1c78901581721938f3113c02aab86394e5f18c26
Sam Greenwood <sam@samgreenwood.me>
2017-07-03 23:17:18 +0930

    Clone repository

--- app/Repository.php
+++ app/Repository.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use Gitonomy\Git\Repository as GitRepository;
 use Illuminate\Database\Eloquent\Model;
 
 class Repository extends Model
 {
+    protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -14,5 +17,34 @@ class Repository extends Model
     public function posts()
     {
         return $this->hasMany(Post::class);
+    }
+
+    public function commits()
+    {
+        return $this->hasMany(Commit::class);
+    }
+
+    public static function fromUrlWithCommits($user, $name, $url)
+    {
+        $tempPath = tempnam(sys_get_temp_dir(), 'branch2blog_');
+        
+        $git = new PHPGit\Git();
+        $git->clone($url, $tempPath);
+
+        $gitRepository = new GitRepository($tempPath);
+
+        $repository = new static(['name' => $url, 'url' => $url]);
+        
+        $user->addRepository($repository);
+
+        foreach($gitRepository->getLog() as $commit)
+        {
+            $repository->commits()->save(new Commit([
+                'hash' => $commit['hash'],
+                'title' => $commit['title'],
+            ]));
+        }
+
+        return $repository;
     }
 }
--- database/migrations/2017_06_27_143058_create_repositories_table.php
+++ database/migrations/2017_06_27_143058_create_repositories_table.php
@@ -15,6 +15,7 @@ class CreateRepositoriesTable extends Migration
     {
         Schema::create('repositories', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->string('url');
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
